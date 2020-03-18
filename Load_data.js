c4a065271cfb6d820126f6adb56ba1f2cc8f5f58
james <gb1223codeisgreat>
2020-03-18 13:14:17 +0000

    just made the data load on desktop

--- Load_data.js
+++ Load_data.js
@@ -2,13 +2,13 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
 
-const currentDataLocation = path.join(__dirname, "./data/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/");
-const archivedDataLocation = path.join(__dirname, "./data/COVID-19/archived_data/archived_daily_case_updates/");
+const currentDataLocation = path.join(__dirname, "./data/csse_covid_19_data/csse_covid_19_daily_reports/");
+
 
 exports.readInData = function(callback){
     var dataarray = [];
     // Process Current Data Files
-    var files = fs.readdirSync(currentDataLocation).forEach(file =>{
+    fs.readdirSync(currentDataLocation).forEach(file =>{
         if(file.endsWith('.csv')){
             var event2 = fs.createReadStream(currentDataLocation + file).pipe(csv()).on('data', data => {
                 dataarray.push(data);
